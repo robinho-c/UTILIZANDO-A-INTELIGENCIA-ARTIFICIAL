a81b82184aaba33198f83b8d12541ce96b23a939
robinho-c <159184830+robinho-c@users.noreply.github.com>
2025-09-02 11:19:43 -0300

    02/09

---
+++ main.js
@@ -0,0 +1,55 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado "pergunta 1",
+    alternativas:[
+        "Alternativa 1",
+        "Alternativa 2",
+    ]
+},
+
+{
+    
+        enunciado: "pergunta 2",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    
+
+    {
+    
+        enunciado: "pergunta 3",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    
+
+
+    {
+    
+        enunciado: "pergunta 4",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    
+
+    {
+    
+        enunciado: "pergunta 5",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+]
